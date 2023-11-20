de2c5496e9161b1198cd1537659e7e75d5aa9fdf
Luna <luna.sabani@gmail.com>
2023-11-20 10:46:35 +0100

    klasa clan koja nam vrv nece trebati al ok

---
+++ Clan.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace WebApplication6
+{
+    public class Clan
+    {
+        public int id { get; set; }
+        public string Ime { get; set; }
+        public string prezime { get; set; }
+        public string kontakt {get;set;}
+        public string broj_telefona { get; set; }
+        public string grad { get; set; }
+
+        public Clan() { }
+
+        
+    }
+}
