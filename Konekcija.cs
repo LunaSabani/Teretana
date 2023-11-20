11377be8dcc68417f381aeef02efc18086f0bb11
Luna <luna.sabani@gmail.com>
2023-11-20 10:25:53 +0100

    klasa konekcije

---
+++ Konekcija.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace WebApplication6
+{
+    public class Konekcija
+    {
+
+        public static string constring = @"Data Source=.\sqlexpress;Integrated Security=True;Initial Catalog=Teretana";    
+}
+}
